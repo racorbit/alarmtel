661cf7a4a9868db872649be7e1fa7fa7b7aeace5
Rick Corbit <rcorbit@gmail.com>
2013-09-27 17:43:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alarmtell::Application.config.secret_token = 'aa9badba49fdd1bbe2337ec61ab6765854020b9369ccf13d7389c12e0c6edcd4bfdfa402d969afe8bd48b31e595235843e4ab15e0d72d4a771034e8d1d214746'
